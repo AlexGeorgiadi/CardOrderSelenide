5982384ed4a1cade4b57253cfc728458643c7100
Alextos <alex-georgiadi@ya.ru>
2023-04-11 20:09:52 +0300

    CardOrderSelenideTest.java modified

--- src/test/java/ru/netology/cardorderselenide/CardOrderSelenideTest.java
+++ src/test/java/ru/netology/cardorderselenide/CardOrderSelenideTest.java
@@ -11,18 +11,23 @@ import static com.codeborne.selenide.Selenide.*;
 
 class CardOrderSelenideTest {
 
+    String planningDate = Date.setDate(3);
+
     @Test
     void shouldRegister() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Нальчик");
-        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.COMMAND + "a");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(Date.setDate(3));
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Георгиади Алексей");
         $("[data-test-id=phone] input").setValue("+79614660837");
         $("[data-test-id=agreement]").click();
         $x("//*[contains(text(),'Забронировать')]").click();
         $("[data-test-id=notification]").should(Condition.appear, Duration.ofSeconds(13));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
